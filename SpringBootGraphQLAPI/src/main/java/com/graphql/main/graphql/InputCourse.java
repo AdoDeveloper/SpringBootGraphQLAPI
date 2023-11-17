3ec4c9cb842976793a1781b4f2eae8740aa931d6
Carlos Reyes <rc21i04001@usonsonate.edu.sv>
2023-11-16 21:50:23 -0600

    Parte del Graphql

--- SpringBootGraphQLAPI/src/main/java/com/graphql/main/graphql/InputCourse.java
+++ SpringBootGraphQLAPI/src/main/java/com/graphql/main/graphql/InputCourse.java
@@ -1,5 +1,30 @@
 package com.graphql.main.graphql;
 
+import lombok.Data;
+
+@Data
 public class InputCourse {
 
+    private String name;
+    private String category;
+    private String teacher;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(String teacher) {
+		this.teacher = teacher;
+	}
+	
 }
--- SpringBootGraphQLAPI/src/main/java/com/graphql/main/graphql/InputStudent.java
+++ SpringBootGraphQLAPI/src/main/java/com/graphql/main/graphql/InputStudent.java
@@ -1,5 +1,43 @@
 package com.graphql.main.graphql;
 
+import lombok.Data;
+
+@Data
 public class InputStudent {
 
+    private Long id;
+    private String name;
+    private String lastName;
+    private Integer age;
+    private String courseId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(String courseId) {
+		this.courseId = courseId;
+	}
 }
