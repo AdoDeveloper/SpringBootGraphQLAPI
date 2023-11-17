3dc9c60ae3cea449f01172588c409614a1c092d1
23GW <130580251+23GW@users.noreply.github.com>
2023-11-17 13:13:52 -0600

    William
    Reviza si lo hice bien ;"v

--- SpringBootGraphQLAPI/src/main/java/com/graphql/main/service/IStudentService.java
+++ SpringBootGraphQLAPI/src/main/java/com/graphql/main/service/IStudentService.java
@@ -1,5 +1,21 @@
-package com.graphql.main.service;
+package com.graphql.example.service;
+
+//import com.graphql.example.entities.Course;
+import com.graphql.example.entities.Student;
+
+import java.util.List;
 
 public interface IStudentService {
 
+    Student findById(Long id);
+
+    List<Student> findAll();
+
+    void createStudent(Student student);
+
+    void deleteById(Long id);
+    
+ // Método para actualizar un estudiante con el curso
+ // Student updateStudent(Long id, String newName, String newLastName, Integer newAge, Course newCourse);
+    void updateStudent(Student updatedStudent);
 }
